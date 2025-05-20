3c4e76e4bb0ed2f2989e9584a232cdc1c6950c97
deVictorS <victorsantos3245@gmail.com>
2025-05-20 14:06:59 -0300

    ex1 completo

--- estruturasHomogeneas/obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ estruturasHomogeneas/obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765f7ed46731533cd33aeb7885c24b964a4d9517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae32bbd67bbcb1990ca2272cce8bca4765810251")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
